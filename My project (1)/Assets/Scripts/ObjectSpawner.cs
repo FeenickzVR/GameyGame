182df01a6832a14ae705042fa6a52a88655e3e23
FeenickzVR <feenickz05@gmail.com>
2024-09-10 02:48:27 -0400

    push
    Various fixes, additions, and changes

--- My project (1)/Assets/Scripts/ObjectSpawner.cs
+++ My project (1)/Assets/Scripts/ObjectSpawner.cs
@@ -7,54 +7,47 @@ public class ObjectSpawner : MonoBehaviour
 {
 
     [SerializeField] private float spawnRate;
+    public float spawnDelay;
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject enemyPrefabs;
     [SerializeField] private bool canSpawn = true;
     GameObject enemyClone;
     float x;
-    float waveTimer;
-    int waveCount;
-    public float minWaitTime = 0.1f; 
-    public float maxWaitTime = 5f; 
-
 
 
     // Start is called before the first frame update
     void Start()
     {
-        waveTimer -= Time.deltaTime;
-        spawnRate = 1f;
+        spawnDelay = spawnRate;
         StartCoroutine(Spawner());
     }
 
     // Update is called once per frame
     void Update()
     {
-        waveTimer -= Time.deltaTime;
-        if (waveTimer <= 0)
-        {
-            waveCount++;
-            waveTimer = 10;
-        }
 
-        
-        
+
     }
 
     private IEnumerator Spawner()
     {
-        float wait = Mathf.Clamp(spawnRate, minWaitTime, maxWaitTime);
 
         while (canSpawn)
         {
-            yield return wait;
+
+                if (spawnDelay > 0.1)
+                {
+                spawnDelay -= 0.05f;
+                }
+                yield return new WaitForSeconds(spawnDelay);
 
             x = player.transform.position.x;
 
             Vector2 spawnPos = new Vector2(Random.Range(x - 4, x + 4f), 5);
             enemyClone = Instantiate(enemyPrefabs, spawnPos, Quaternion.identity);
             enemyClone.transform.Rotate(0, 0, 180);
-            
+
+            Destroy(enemyClone, 2.5f);
 
         }
     }
